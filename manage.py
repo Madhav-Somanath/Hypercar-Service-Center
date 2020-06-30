e271f273adb49fb201a9696682da2f137781e9e6
Madhav Somanath <madhav.somanath@gmail.com>
2020-06-30 14:52:29 +0530

    Add files via upload

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HypercarService.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hypercar.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
